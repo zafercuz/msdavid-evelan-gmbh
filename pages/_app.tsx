f8f93d21a909725b0996786373e699fc95ac5a8f
zafercuz <beltondavid1@gmail.com>
2023-03-23 14:56:54 +0800

    My Initial Preparation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Test</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
